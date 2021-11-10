b4ab812382d5d8724a575a54a4bd5e241a821a77
Nikolos123 <Isponec2>
2021-11-10 22:16:53 +0300

    add two controller and urls

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from mainapp.views import index,products
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index,name='name'),
+    path('products/', products,name='products'),
+
 ]
