6e0cec3c632854dff17bcda1f2725649f82c8075
Nikolos123 <Isponec2>
2021-12-05 14:15:04 +0300

    lesson_8_end

--- geekshop/admins/urls.py
+++ geekshop/admins/urls.py
@@ -1,13 +1,13 @@
 
 from django.urls import path
 
-from admins.views import index, UserCreateView, UserUpdateView, UserDeleteView, UserListView, \
+from admins.views import IndexTemplateView, UserCreateView, UserUpdateView, UserDeleteView, UserListView, \
     CategoryListView, CategoryDeleteView, CategoryUpdateView, ProductListView, CategoryCreateView
 
 app_name = 'admins'
 urlpatterns = [
 
-    path('', index,name='index'),
+    path('', IndexTemplateView.as_view(),name='index'),
     path('users/', UserListView.as_view(),name='admin_users'),
     path('users-create/', UserCreateView.as_view(),name='admin_users_create'),
     path('users-update/<int:pk>', UserUpdateView.as_view(),name='admin_users_update'),
--- geekshop/admins/views.py
+++ geekshop/admins/views.py
@@ -13,58 +13,35 @@ from mainapp.mixin import BaseClassContextMixin, CustomDispatchMixin
 from mainapp.models import Product, ProductCategory
 
 
-@user_passes_test(lambda u: u.is_superuser)
-def index(request):
-    return render(request, 'admins/admin.html')
+class IndexTemplateView(TemplateView):
+    template_name = 'admins/admin.html'
 
-class UserListView(ListView):
+#Users
+class UserListView(ListView,BaseClassContextMixin,CustomDispatchMixin):
     model = User
     template_name = 'admins/admin-users-read.html'
+    title = 'Админка | Пользователи'
 
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super(UserListView, self).get_context_data(**kwargs)
-        context['title'] = 'Админка | Пользователи'
-        return context
-
-    @method_decorator(user_passes_test(lambda u: u.is_superuser))
-    def dispatch(self, request, *args, **kwargs):
-        return super(UserListView, self).dispatch(request, *args, **kwargs)
-
-class UserCreateView(CreateView):
+class UserCreateView(CreateView,BaseClassContextMixin,CustomDispatchMixin):
     model = User
     template_name = 'admins/admin-users-create.html'
     form_class = UserAdminRegisterForm
     success_url = reverse_lazy('admins:admin_users')
+    title = 'Админка | Создать пользователя'
 
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super(UserCreateView, self).get_context_data(**kwargs)
-        context['title'] = 'Админка | Создать пользователя'
-        return context
-
-    @method_decorator(user_passes_test(lambda u: u.is_superuser))
-    def dispatch(self, request, *args, **kwargs):
-        return super(UserCreateView, self).dispatch(request, *args, **kwargs)
-
-class UserUpdateView(UpdateView):
+class UserUpdateView(UpdateView,BaseClassContextMixin,CustomDispatchMixin):
     model = User
     template_name = 'admins/admin-users-update-delete.html'
     form_class = UserAdminProfileForm
     success_url = reverse_lazy('admins:admin_users')
+    title = 'Админка | Обновить пользователя'
 
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super(UserUpdateView, self).get_context_data(**kwargs)
-        context['title'] = 'Админка | Обновить пользователя'
-        return context
-
-    @method_decorator(user_passes_test(lambda u: u.is_superuser))
-    def dispatch(self, request, *args, **kwargs):
-        return super(UserUpdateView, self).dispatch(request, *args, **kwargs)
-
-class UserDeleteView(DeleteView):
+class UserDeleteView(DeleteView,BaseClassContextMixin,CustomDispatchMixin):
     model = User
     template_name = 'admins/admin-users-update-delete.html'
     form_class = UserAdminProfileForm
     success_url = reverse_lazy('admins:admin_users')
+    title = 'Админка | Удалить пользователя'
 
     def delete(self, request, *args, **kwargs):
         self.object =self.get_object()
@@ -72,23 +49,13 @@ class UserDeleteView(DeleteView):
         self.object.save()
         return HttpResponseRedirect(self.get_success_url())
 
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super(UserDeleteView, self).get_context_data(**kwargs)
-        context['title'] = 'Админка | Удалить пользователя'
-        return context
-
-    @method_decorator(user_passes_test(lambda u: u.is_superuser))
-    def dispatch(self, request, *args, **kwargs):
-        return super(UserDeleteView, self).dispatch(request, *args, **kwargs)
-
 # Category
-
 class CategoryListView(ListView,BaseClassContextMixin,CustomDispatchMixin):
     model = ProductCategory
     template_name = 'admins/admin-category-read.html'
     title = 'Админка | Список категорий'
 
-class CategoryDeleteView(DeleteView,CustomDispatchMixin):
+class CategoryDeleteView(DeleteView,BaseClassContextMixin,CustomDispatchMixin):
     model = ProductCategory
     template_name = 'admins/admin-category-update-delete.html'
     success_url = reverse_lazy('admins:admin_category')
